ec19e532ca0df8861841c37896e5cd6a8c3849f6
Aashish17405 <aashish17405@gmail.com>
2024-08-18 00:10:13 +0530

    .

--- server/db.js
+++ server/db.js
@@ -1,45 +1,47 @@
-const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://Aashish17405:Aashish17@cluster0.muslifi.mongodb.net/Library-Management');
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: String,
     password: String
-})
+});
 
-const Users=mongoose.model('Users',userSchema);
+const Users = mongoose.model('Users', userSchema);
 
 const bookSchema = new mongoose.Schema({
-    name:String,
-    available:Number,
-    author:String,
-    publicationYear:Number,
+    name: String,
+    available: Number,
+    author: String,
+    publicationYear: Number,
     img: {
         data: Buffer,
         contentType: String,
     }
-})
+});
 
-const Book = mongoose.model('Book',bookSchema);
+const Book = mongoose.model('Book', bookSchema);
 
 const borrowerSchema = new mongoose.Schema({
     book: String,
     name: String,
     phone: Number,
     borrowedDateTime: String,
-})
+});
 
-const Borrower = mongoose.model('Borrower',borrowerSchema);
+const Borrower = mongoose.model('Borrower', borrowerSchema);
 
-const ReturnerSchema = new mongoose.Schema({
+const returnerSchema = new mongoose.Schema({
     book: String,
     name: String,
     phone: Number,
     borrowedDateTime: String,
-    returnedDateTime:String,
-})
+    returnedDateTime: String,
+});
 
-const Returner = mongoose.model('Returner',ReturnerSchema);
+const Returner = mongoose.model('Returner', returnerSchema);
 
 module.exports = {
-    Book,Borrower,Users,Returner
+    Book,
+    Borrower,
+    Users,
+    Returner
 };
