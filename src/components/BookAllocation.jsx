05dc054d11ea851a042a2f41dbe3a541f7158352
Aashish17405 <aashish17405@gmail.com>
2024-08-18 11:38:08 +0530

    .

--- src/components/BookAllocation.jsx
+++ src/components/BookAllocation.jsx
@@ -115,10 +115,11 @@ function DataTable() {
 
     return (
         <div className='bg-gray-50 min-h-screen flex flex-col'>
-            <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50">
+
+            {loading && <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50">
                 <Navbar />
-                {loading && <Lottie options={defaultOptions} height={250} width={250}/>}
-            </div>
+                <Lottie options={defaultOptions} height={250} width={250}/>
+            </div>}
 
             {!loading && (
                 <div>
--- src/components/ReturnDetails.jsx
+++ src/components/ReturnDetails.jsx
@@ -80,10 +80,10 @@ function ReturnDetails() {
     return (
         <div className='bg-gray-50 min-h-screen flex flex-col'>
             
-            <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50">
+            {loading && <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50">
                 <Navbar />
-                {loading && <Lottie options={defaultOptions} height={250} width={250}/>}
-            </div>
+                <Lottie options={defaultOptions} height={250} width={250}/>
+            </div>}
 
             {!loading && (
                 <>
